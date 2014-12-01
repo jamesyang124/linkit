155d90d55b734cb143b8482612092fa1b8f78f8e
James Yang <jamesyang124@gmail.com>
2014-12-01 13:08:18 -0800

    mock and pass omniauth registration test for User model.

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -2,5 +2,4 @@ Fabricator(:user) do
   name { "John Doe" }
   email { "johndoe@email.com" }
   password { "johndoepwd" }
-  id 1
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,11 +6,20 @@ describe User do
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
 
-  content "register through site" do
-    
+  
+  it "allowed register without omniauth." do
+    user = Fabricate(:user)
+    expect(User.last).to eq(user)
   end
 
-  context "register through OmniAuth" do
-    
+  it "reigster through omniauth" do
+    request_auth = double(:request_auth)
+    allow(request_auth).to receive_messages(:provider => "facebook", :uid => "uid")
+    allow(request_auth).to receive_message_chain("info.email") { "info_email@info.com" }
+    allow(request_auth).to receive_message_chain("info.name") { "Info Name" }
+    allow(request_auth).to receive_message_chain("info.image") { "http://info.mage" }
+
+    user = User.from_omniauth(request_auth)
+    expect(User.last).to eq(user)
   end
 end
